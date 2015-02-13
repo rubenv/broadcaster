60e63d24fed56490088414ba4293d8645c2bb0b3
Ruben Vermeersch <ruben@rocketeer.be>
2015-02-13 16:52:49 +0100

    Test CanSubscribe

--- websocket_test.go
+++ websocket_test.go
@@ -128,3 +128,45 @@ func TestWSSubscribe(t *testing.T) {
 		t.Errorf("Unexpected subscription count: %d", stats.localSubscriptions["test"])
 	}
 }
+
+func TestWSCanSubscribe(t *testing.T) {
+	server, err := startServer(&Server{
+		CanSubscribe: func(data map[string]string, channel string) bool {
+			return false
+		},
+	}, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Stop()
+
+	client, err := newWSClient(server)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.Authenticate(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.Subscribe("test")
+	if err == nil {
+		t.Fatal("Expected error!")
+	}
+	if err.Error() != "websocket: close 403 Channel refused" {
+		t.Fatal("Did not properly deny access")
+	}
+
+	stats, err := server.Broadcaster.Stats()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if stats.Connections != 0 {
+		// Should disconnect bad client
+		t.Errorf("Unexpected connection count: %d", stats.Connections)
+	}
+	if stats.localSubscriptions["test"] != 0 {
+		t.Errorf("Unexpected subscription count: %d", stats.localSubscriptions["test"])
+	}
+}
