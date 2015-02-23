98cb61e6e55fa4335cce063451082cf3af52df90
Ruben Vermeersch <ruben@rocketeer.be>
2015-02-23 14:23:44 +0100

    Clean up test suite, a lot.

--- client_test.go
+++ client_test.go
@@ -14,28 +14,6 @@ func init() {
 	//	}()
 }
 
-func testConnect(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	stats, err := server.Broadcaster.Stats()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if stats.Connections != 1 {
-		t.Errorf("Unexpected connection count: %d", stats.Connections)
-	}
-}
-
 func testCanConnect(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
 	server, err := startServer(&Server{
 		CanConnect: func(data map[string]interface{}) bool {
@@ -64,7 +42,7 @@ func testCanConnect(t *testing.T, clientFn func(s *testServer, conf ...func(c *C
 	}
 }
 
-func testAuthData(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
+func testClient(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
 	server, err := startServer(&Server{
 		CanConnect: func(data map[string]interface{}) bool {
 			return data["token"] == "abcdefg"
@@ -75,13 +53,118 @@ func testAuthData(t *testing.T, clientFn func(s *testServer, conf ...func(c *Cli
 	}
 	defer server.Stop()
 
+	// Connect + authenticate
 	client, err := clientFn(server, func(c *Client) {
 		c.AuthData = map[string]interface{}{"token": "abcdefg"}
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	client.Disconnect()
+	defer client.Disconnect()
+
+	stats, err := server.Broadcaster.Stats()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if stats.Connections != 1 {
+		t.Errorf("Unexpected connection count: %d", stats.Connections)
+	}
+
+	// Subscribe
+	err = client.Subscribe("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ready := false
+	for !ready {
+		stats, _ := server.Broadcaster.Stats()
+		if stats.LocalSubscriptions["test"] != 1 {
+			// Wait until polling socket is connected. There can be a small gap between
+			// connecting and listening while long-polling.
+			<-time.After(100 * time.Millisecond)
+		} else {
+			ready = true
+		}
+	}
+
+	stats, err = server.Broadcaster.Stats()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if stats.Connections != 1 {
+		t.Errorf("Unexpected connection count: %d", stats.Connections)
+	}
+	if stats.LocalSubscriptions["test"] != 1 {
+		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
+	}
+
+	// Shouldn't get message on other channel
+	err = server.sendMessage("other", "Other message")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Receive messages
+	err = server.sendMessage("test", "Test message")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = server.sendMessage("test", "Test message 2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m := <-client.Messages
+	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message" {
+		t.Error("Wrong message payload")
+	}
+
+	m = <-client.Messages
+	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message 2" {
+		t.Error("Wrong message payload")
+	}
+
+	// Wait until next polling interval (tests follow-up connections)
+	<-time.After(100 * time.Millisecond)
+	err = server.sendMessage("test", "Test message 3")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m = <-client.Messages
+	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message 3" {
+		t.Error("Wrong message payload")
+	}
+
+	// Unsubscribe
+	err = client.Unsubscribe("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Wait for client to catch up
+	ready = false
+	for !ready {
+		stats, _ := server.Broadcaster.Stats()
+		if stats.LocalSubscriptions["test"] != 0 {
+			<-time.After(100 * time.Millisecond)
+		} else {
+			ready = true
+		}
+	}
+
+	stats, err = server.Broadcaster.Stats()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if stats.Connections != 1 {
+		t.Errorf("Unexpected connection count: %d", stats.Connections)
+	}
+	if stats.LocalSubscriptions["test"] != 0 {
+		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
+	}
 }
 
 func testRefusesUnauthedCommands(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
@@ -121,40 +204,6 @@ func testRefusesUnauthedCommands(t *testing.T, clientFn func(s *testServer, conf
 	}
 }
 
-func testSubscribe(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	err = client.Subscribe("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Wait until polling socket is connected. This makes sure that the local
-	// subscription count is correct.
-	<-time.After(100 * time.Millisecond)
-
-	stats, err := server.Broadcaster.Stats()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if stats.Connections != 1 {
-		t.Errorf("Unexpected connection count: %d", stats.Connections)
-	}
-	if stats.LocalSubscriptions["test"] != 1 {
-		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
-	}
-}
-
 func testCanSubscribe(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
 	server, err := startServer(&Server{
 		CanSubscribe: func(data map[string]interface{}, channel string) bool {
@@ -183,194 +232,6 @@ func testCanSubscribe(t *testing.T, clientFn func(s *testServer, conf ...func(c 
 	stats, err := server.Broadcaster.Stats()
 	if err != nil {
 		t.Fatal(err)
-	}
-	if stats.LocalSubscriptions["test"] != 0 {
-		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
-	}
-}
-
-func testMessageTypes(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	err = client.send("bla", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m, err := client.receive()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if m.Type() != UnknownMessage {
-		t.Fatal("Did not properly refuse message type")
-	}
-
-	stats, err := server.Broadcaster.Stats()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if stats.LocalSubscriptions["test"] != 0 {
-		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
-	}
-}
-
-func testMessage(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	err = client.Subscribe("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ready := false
-	for !ready {
-		stats, _ := server.Broadcaster.Stats()
-		if stats.LocalSubscriptions["test"] != 1 {
-			// Wait until polling socket is connected. There can be a small gap between
-			// connecting and listening while long-polling.
-			<-time.After(100 * time.Millisecond)
-		} else {
-			ready = true
-		}
-	}
-
-	err = server.sendMessage("test", "Test message")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = server.sendMessage("test", "Test message 2")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m := <-client.Messages
-	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message" {
-		t.Error("Wrong message payload")
-	}
-
-	m = <-client.Messages
-	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message 2" {
-		t.Error("Wrong message payload")
-	}
-
-	// Wait until next polling interval (tests follow-up connections)
-	<-time.After(100 * time.Millisecond)
-	err = server.sendMessage("test", "Test message 3")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m = <-client.Messages
-	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message 3" {
-		t.Error("Wrong message payload")
-	}
-}
-
-func testMessageChannel(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	err = client.Subscribe("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ready := false
-	for !ready {
-		stats, _ := server.Broadcaster.Stats()
-		if stats.LocalSubscriptions["test"] != 1 {
-			// Wait until polling socket is connected. There can be a small gap between
-			// connecting and listening while long-polling.
-			<-time.After(100 * time.Millisecond)
-		} else {
-			ready = true
-		}
-	}
-
-	err = server.sendMessage("other", "Test message")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = server.sendMessage("test", "Test message")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m := <-client.Messages
-	if m.Type() != "message" || m["channel"] != "test" || m["body"] != "Test message" {
-		t.Error("Wrong message payload")
-	}
-
-	select {
-	case <-client.Messages:
-		t.Error("Unexpected message")
-	default:
-	}
-}
-
-func testUnsubscribe(t *testing.T, clientFn func(s *testServer, conf ...func(c *Client)) (*Client, error)) {
-	server, err := startServer(nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Stop()
-
-	client, err := clientFn(server)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Disconnect()
-
-	err = client.Subscribe("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = client.Unsubscribe("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Wait for client to catch up
-	<-time.After(100 * time.Millisecond)
-
-	stats, err := server.Broadcaster.Stats()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if stats.Connections != 1 {
-		t.Errorf("Unexpected connection count: %d", stats.Connections)
 	}
 	if stats.LocalSubscriptions["test"] != 0 {
 		t.Errorf("Unexpected subscription count: %d", stats.LocalSubscriptions["test"])
--- websocket_test.go
+++ websocket_test.go
@@ -2,42 +2,18 @@ package broadcaster
 
 import "testing"
 
-func TestWSConnect(t *testing.T) {
-	testConnect(t, newWSClient)
+func TestWSClient(t *testing.T) {
+	testClient(t, newWSClient)
 }
 
 func TestWSCanConnect(t *testing.T) {
 	testCanConnect(t, newWSClient)
 }
 
-func TestWSAuthData(t *testing.T) {
-	testAuthData(t, newWSClient)
-}
-
 func TestWSRefusesUnauthedCommands(t *testing.T) {
 	testRefusesUnauthedCommands(t, newWSClient)
 }
 
-func TestWSSubscribe(t *testing.T) {
-	testSubscribe(t, newWSClient)
-}
-
 func TestWSCanSubscribe(t *testing.T) {
 	testCanSubscribe(t, newWSClient)
-}
-
-func TestWSMessageTypes(t *testing.T) {
-	testMessageTypes(t, newWSClient)
-}
-
-func TestWSMessage(t *testing.T) {
-	testMessage(t, newWSClient)
-}
-
-func TestWSMessageChannel(t *testing.T) {
-	testMessageChannel(t, newWSClient)
-}
-
-func TestWSUnsubscribe(t *testing.T) {
-	testUnsubscribe(t, newWSClient)
 }
